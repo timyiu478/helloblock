5c8f55ed86615af6cf08f459f77d60ec567df03b
Kam Wing Yiu <ctom3560@gmail.com>
2025-04-28 21:29:58 +0800

    add python script to call the Hello()

---
+++ main.py
@@ -0,0 +1,27 @@
+#!/usr/local/bin/python3
+
+from web3 import Web3
+from eth_abi import decode
+
+# Connect to the Ethereum network
+web3 = Web3(Web3.HTTPProvider('PLACE_HOLDER'))
+
+# Function signature for SayHello()
+function_signature = "SayHello()"
+
+# Encode the function signature to get the function selector
+function_selector = web3.keccak(text=function_signature)[:4].hex()
+
+# Query the blockchain (replace example parameters)
+data = web3.eth.call({
+		'value': 0, 
+		'gas': 2173600, 
+		'maxFeePerGas': 40000000000, 
+		'maxPriorityFeePerGas': 1000000000, 
+		'to': '0xe35EabbF16226BEda3fde7Bffca24890FCcB24eC',
+		'data': function_selector
+		}) 
+
+response = decode(['string'], data)[0]
+
+print(response)
